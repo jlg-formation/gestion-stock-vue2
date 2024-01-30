8282225817272024ccf2a9d81d556f9b52545bdb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-30 19:58:19 +0100

    dev: added scss

--- front/src/components/layout/HeaderLayout.vue
+++ front/src/components/layout/HeaderLayout.vue
@@ -15,12 +15,16 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 header {
   background: #eee;
   height: 3em;
   padding: 0 0.5em;
   display: flex;
   align-items: center;
+
+  img {
+    height: 2.5em;
+  }
 }
 </style>
