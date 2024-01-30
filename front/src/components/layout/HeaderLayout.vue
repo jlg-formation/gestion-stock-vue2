8ef6f55f2738494f1daad184b703ecb69428e449
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-30 19:55:37 +0100

    dev: added header layout and global style

---
+++ front/src/components/layout/HeaderLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <a href="#">
+      <img src="assets/logo.svg" alt="Logo Gestion Stock" />
+      <span>Gestion Stock</span>
+    </a>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderLayout",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  background: #eee;
+  height: 3em;
+  padding: 0 0.5em;
+  display: flex;
+  align-items: center;
+}
+</style>
