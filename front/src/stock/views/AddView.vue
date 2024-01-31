8d5ed46a3d24acd37e8c2936e3ce164259ff8f95
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-31 13:52:59 +0100

    dev: added form

--- front/src/stock/views/AddView.vue
+++ front/src/stock/views/AddView.vue
@@ -7,10 +7,53 @@ export default {
 <template>
   <main>
     <h1>Ajouter un article</h1>
-    <router-link to="/stock" class="button primary">
-      Revenir à la liste
-    </router-link>
+    <form>
+      <label>
+        <span>Nom</span>
+        <input type="text" />
+        <span class="error"></span>
+      </label>
+      <label>
+        <span>Prix</span>
+        <input type="number" />
+        <span class="error"></span>
+      </label>
+      <label>
+        <span>Quantité</span>
+        <input type="number" />
+        <span class="error"></span>
+      </label>
+      <div class="error"></div>
+      <button class="primary">Ajouter</button>
+    </form>
   </main>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+form {
+  display: flex;
+  flex-flow: column;
+  gap: 0.5em;
+
+  width: 100%;
+  max-width: 25em;
+
+  label {
+    display: flex;
+    flex-flow: column;
+
+    input {
+      padding: 0.5em 1em;
+      border: 0.1em solid #aaa;
+      border-radius: 0.3em;
+    }
+  }
+
+  div.error {
+    height: 3em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
