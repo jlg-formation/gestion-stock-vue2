a490ed436d886818c535b20754b2f37ddf455624
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-31 17:46:11 +0100

    dev: remove log on production

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import App from "./App.vue";
+import { isProd } from "@/utils/misc";
 
 import "./styles.scss";
 import router from "./router";
@@ -8,6 +9,10 @@ import router from "./router";
 import "./icons/fontawesome";
 
 Vue.config.productionTip = false;
+
+if (isProd()) {
+  console.log = () => {};
+}
 
 Vue.use(Vuex);
 
--- front/src/utils/misc.js
+++ front/src/utils/misc.js
@@ -1,2 +1,4 @@
 export const sleep = (delay) =>
   new Promise((resolve) => setTimeout(resolve, delay));
+
+export const isProd = () => process.env.NODE_ENV !== "development";
