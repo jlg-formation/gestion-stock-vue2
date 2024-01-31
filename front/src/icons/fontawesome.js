bcff29a7bb315bf702537af4328018e0b5d6824e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-31 16:18:02 +0100

    dev: more specific on fontawesome icons
    Decreasing the production size

--- front/src/icons/fontawesome.js
+++ front/src/icons/fontawesome.js
@@ -7,10 +7,16 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleRight,
+  faPlus,
+  faRotateRight,
+  faTrashCan,
+  faCircleNotch,
+} from "@fortawesome/free-solid-svg-icons";
 
 /* add icons to the library */
-library.add(fas);
+library.add(faAngleRight, faPlus, faRotateRight, faTrashCan, faCircleNotch);
 
 /* add font awesome icon component */
 Vue.component("fa-icon", FontAwesomeIcon);
