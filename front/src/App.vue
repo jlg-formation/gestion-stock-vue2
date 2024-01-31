6e1cb6604c82c7c064a98e385f07b7498737e6d2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-31 09:40:39 +0100

    dev: install vue-router@3 for vue 2
    refactored a little bit vue router App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <HeaderLayout />
     <BodyLayout />
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
     <FooterLayout />
   </div>
 </template>
