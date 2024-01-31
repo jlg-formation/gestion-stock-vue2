8ebbe3c0b11835821583d1f599b6b767faef9d2f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-31 12:33:30 +0100

    dev: added listview dom and css

--- front/src/stock/views/ListView.vue
+++ front/src/stock/views/ListView.vue
@@ -7,10 +7,88 @@ export default {
 <template>
   <main>
     <h1>Liste des articles</h1>
-    <router-link to="/stock/add" class="button primary">
-      Ajouter un article
-    </router-link>
+    <div class="content">
+      <nav>
+        <button>Rafraîchir</button>
+        <router-link append to="add" class="button">Ajouter</router-link>
+        <button>Supprimer</button>
+      </nav>
+      <div class="error"></div>
+      <table>
+        <thead>
+          <th class="name">Nom</th>
+          <th class="price">Prix</th>
+          <th class="qty">Quantité</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="name">Tournevis</td>
+            <td class="price">2.99 €</td>
+            <td class="qty">240</td>
+          </tr>
+          <tr>
+            <td class="name">Pelle</td>
+            <td class="price">10.00 €</td>
+            <td class="qty">240</td>
+          </tr>
+          <tr>
+            <td class="name">Marteau</td>
+            <td class="price">4.50 €</td>
+            <td class="qty">240</td>
+          </tr>
+          <tr>
+            <td class="name">Ponçeuse</td>
+            <td class="price">55.00 €</td>
+            <td class="qty">240</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </main>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+nav {
+  display: flex;
+  gap: 0.5em;
+}
+
+div.error {
+  height: 2em;
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+}
+
+table {
+  border: 0.1em solid #aaa;
+  border-collapse: separate;
+  border-spacing: 0;
+  border-radius: 0.3em;
+  overflow: hidden;
+
+  th,
+  td {
+    padding: 0.5em 1em;
+  }
+
+  thead {
+    background: #aaa;
+  }
+
+  tbody {
+    cursor: pointer;
+    tr:nth-child(even) {
+      background: #eee;
+    }
+
+    tr:hover {
+      background: #ddd;
+    }
+
+    tr.selected {
+      background: #ccc;
+    }
+  }
+}
+</style>
