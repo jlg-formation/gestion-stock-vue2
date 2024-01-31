eb5a971997715b912ff5f4507de64db237818392
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-31 09:29:57 +0100

    dev: come back to original settings
    to optimize the size and the load time of production app.

--- front/src/main.js
+++ front/src/main.js
@@ -1,29 +1,10 @@
 import Vue from "vue";
-// import App from "./App.vue";
+import App from "./App.vue";
 
 import "./styles.scss";
 
 Vue.config.productionTip = false;
 
-// new Vue({
-//   render: (h) => h(App),
-// }).$mount("#app");
-
 new Vue({
-  // render: (h) => h(App),
-  el: "#app",
-  // render: (h) => h(App),
-  //   components: {
-  //     App,
-  //   },
-  data: function () {
-    return {
-      firstName: "Ryan",
-    };
-  },
-  template: `
-  <div>
-  <p>Hello truc</p>
-  </div>
-  `,
-});
+  render: (h) => h(App),
+}).$mount("#app");
